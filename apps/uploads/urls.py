ed869968b4a3215824b1d1208b70ef48210aced1
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-07-08 18:44:20 -0300

    refactor: updated urls uploads

--- apps/uploads/urls.py
+++ apps/uploads/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from . import views
+
 app_name = 'uploads'
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.UploadedFileListView.as_view(), name="list"),
+    path("new/", views.UploadedFileCreateView.as_view(), name="create"),
+    path("<int:pk>/delete", views.UploadedFileDeleteView.as_view(), name="delete"),
+]
