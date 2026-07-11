952ccb0f3cbf2ada44004082ebc96ca6bfc785a4
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-07-11 00:53:01 -0300

    fix: import right ettings in wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "core.settings.prod",
+)
 
 application = get_wsgi_application()
+
